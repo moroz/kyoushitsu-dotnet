99b6e120c056c07da0447f61786265b1834f38c6
Karol Moroz <karol@moroz.dev>
2025-07-11 20:25:18 +0800

    Try to define the projects as in the book

--- MyFirstEfCoreApp/Program.cs
+++ MyFirstEfCoreApp/Program.cs
@@ -1,3 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+Console.WriteLine("Hello, world!");
+
+public class Book
+{
+    public int BookId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime PublishedOn { get; set; }
+    public int AuthorId { get; set; }
+    public Author Author { get; set; }
+}
+
+public class Author
+{
+    public int AuthorId { get; set; }
+    public string Name { get; set; }
+    public string WebUrl { get; set; }
+}
+
+public class AppDbContext : DbContext
+{
+    private const string ConnectionString =
+        @"Host=localhost; Database=my_first_ef_core_app_dev; Username=postgres; Password=postgres";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(ConnectionString);
+    }
+
+    public DbSet<Book> Books { get; set; }
+}
